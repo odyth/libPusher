084b42d63508b29d4102a10d2dd3df8cde0d2258
odyth <jstofle@gmail.com>
2016-10-09 02:25:35 -0700

    xcode 8 changes

--- Pods/SocketRocket/SocketRocket/SRWebSocket.m
+++ Pods/SocketRocket/SocketRocket/SRWebSocket.m
@@ -467,7 +467,11 @@ static __strong NSData *CRLFCRLF;
     CFHTTPMessageSetHeaderFieldValue(request, CFSTR("Host"), (__bridge CFStringRef)(_url.port ? [NSString stringWithFormat:@"%@:%@", _url.host, _url.port] : _url.host));
         
     NSMutableData *keyBytes = [[NSMutableData alloc] initWithLength:16];
-    SecRandomCopyBytes(kSecRandomDefault, keyBytes.length, keyBytes.mutableBytes);
+    int result = SecRandomCopyBytes(kSecRandomDefault, keyBytes.length, keyBytes.mutableBytes);
+    if(result < 0)
+    {
+        NSLog(@"socket rocket failed to connect %d", result);
+    }
     
     if ([keyBytes respondsToSelector:@selector(base64EncodedStringWithOptions:)]) {
         _secKey = [keyBytes base64EncodedStringWithOptions:0];
@@ -1328,7 +1332,11 @@ static const size_t SRFrameHeaderOverhead = 32;
         }
     } else {
         uint8_t *mask_key = frame_buffer + frame_buffer_size;
-        SecRandomCopyBytes(kSecRandomDefault, sizeof(uint32_t), (uint8_t *)mask_key);
+        int result = SecRandomCopyBytes(kSecRandomDefault, sizeof(uint32_t), (uint8_t *)mask_key);
+        if(result < 0)
+        {
+            NSLog(@"socket rocket failed to connect %d", result);
+        }
         frame_buffer_size += sizeof(uint32_t);
         
         // TODO: could probably optimize this with SIMD
